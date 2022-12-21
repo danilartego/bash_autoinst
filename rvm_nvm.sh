f4991070ac502f99303b5dae936832e82005a3db
dn.artego <dn.artego@gmail.com>
2022-12-21 22:20:17 +0300

    update

--- rvm_nvm.sh
+++ rvm_nvm.sh
@@ -22,7 +22,7 @@ npm install yarn --global
 source ~/.bashrc
 
 # Не создавать документацию при скачивании Gem's
-echo 'gem: --no-document' >> ~/.gemrc
+# echo 'gem: --no-document' >> ~/.gemrc
 
 # Установка RUBY
 rvm install 3.1.2
